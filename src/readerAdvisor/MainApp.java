6e101fe03a642570ce388c0618ff0acc499a73c5
Samuel E. Sanchez <samuel.ed.sanchez@gmail.com>
2013-12-16 00:15:20 -0500

    Updating all the project - Installer successfully created

--- src/readerAdvisor/MainApp.java
+++ src/readerAdvisor/MainApp.java
@@ -7,7 +7,6 @@ import javax.swing.*;
 
 //TODO: Create a Logger class - http://www.vogella.com/articles/Logging/article.html
 public class MainApp {
-    public static final String SOFTWARE_VERSION = "1.0";
     // To run the class from the command line or from an IDE
     // VM Options : -server -XX:+PrintGCDetails -Dfrontend=epFrontEnd -Dmicrophone[keepLastAudio]=true -DconfigurationFile=script/software.properties
     public static void main(String[] args){
